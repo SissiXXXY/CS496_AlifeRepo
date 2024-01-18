42ddfe303369b31c5df3614ba5f88f069056f926
Yao Xiao <sissixy717@gmail.com>
2024-01-18 09:56:27 -0600

    sim 1

--- simulate.py
+++ simulate.py
@@ -1,1 +1,8 @@
-pass
+import dm_control.mujoco
+import mujoco.viewer
+
+m = dm_control.mujoco.MjModel.from_xml_path("example.xml")
+d = dm_control.mujoco.MjData(m)
+viewer.launch_passive_viewer(m, d)
+
+viewer.close()
