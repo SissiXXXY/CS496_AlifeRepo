76ffb8f085f485410a138d0824bd9662aac1d64f
SissiXXXY <sissixy717@gmail.com>
2024-03-14 15:20:52 -0500

    HW3 Randomness

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CS496_AlifeRepo
 Northwestern CS496 Artificial Life Repository
+
+Delete the fitness.txt before you run
+Run the file : xmlgenerator.py
+It will automatically randomly generate 5 creatures, each will be under the name "new_creature(1-5).xml"
+A fitness.txt file will be generated marking the fitness scores of each creature.
--- script.py
+++ script.py
@@ -8,7 +8,7 @@ d = dm_control.mujoco.MjData(m)
 viewer = mujoco_viewer.MujocoViewer(m, d)
 
 motors = m.nu
-step = 1
+step = 3
 d.ctrl[:motors] = step
 duration_per_direction = 500
 
@@ -16,12 +16,12 @@ for i in range(10000):
     current_step = i // duration_per_direction
     if viewer.is_alive:
         if i % 2 == 0:
-            d.ctrl[:3] = step * 100
-            d.ctrl[4:] = -step * 100
+            d.ctrl[:3] = step * 1000
+            d.ctrl[4:] = -step * 1000
             mujoco.mj_step(m, d)
         else:
-            d.ctrl[:3] = -step * 100
-            d.ctrl[4:] = step * 100
+            d.ctrl[:3] = -step * 1000
+            d.ctrl[4:] = step * 1000
             mujoco.mj_step(m, d)
         viewer.render()
     else:
